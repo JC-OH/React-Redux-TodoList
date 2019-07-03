646c7c9d73dadfe54b2cbeeef77e2091f2432e3c
Jicheol Oh <jicheol@gmail.com>
2019-07-03 10:16:54 -0700

    create Todo component.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,3 +2,25 @@
 //  - text: string is the text to show.
 //  - completed: boolean is whether the todo should appear crossed out.
 //  - onClick() is a callback to invoke when the todo is clicked.
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Todo = ({}) => (
+    <li
+        onClick={onClick}
+        style={{
+            textDecoration: completed ? 'line-through' : 'none'
+        }}
+    >
+    {text}
+    </li>
+)
+
+Todo.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default Todo
